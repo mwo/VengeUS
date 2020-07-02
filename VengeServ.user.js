254c84f0b383a69d7e3ef6c1714a7ee77289aadb
mwo <57203179+mwo@users.noreply.github.com>
2020-07-02 15:37:48 -0700

    Create VengeServ.user.js

---
+++ VengeServ.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         VengeServe
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  bruh
+// @author       You
+// @match        *://venge.io/*
+// @run-at       document-start
+// @grant        none
+// ==/UserScript==
+
+const gameServerURL = 'wss://krone_rgb.pit.red';
+
+window.WebSocket = class extends WebSocket {
+    constructor() {
+        let url = arguments[0],
+            bool = url.includes('server');
+        console.log(bool, url);
+        if (bool) {
+            arguments[0] = gameServerURL;
+        }
+        super(...arguments);
+    }
+}
